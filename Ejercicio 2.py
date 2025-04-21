d948fbae53325d40c27e5f9629e2fb74ced831c2
Jerónimo Cardona Restrepo <116123393+Jrestrepo18@users.noreply.github.com>
2025-04-21 18:03:00 -0500

    Add files via upload

---
+++ Ejercicio 2.py
@@ -0,0 +1,7 @@
+numero= float(input("Ingrese un numero: "))
+if numero > 0:
+    print("Su numero es positivo")
+elif numero < 0:
+    print("Su numero es negativo")
+else:
+    print("Su numero es cero")
